56a428271a9bab9e630f8d0b592e021fa7f90cd1
romio-krvtsky <romik.krivetskiy@gmail.com>
2022-06-01 04:52:22 +0300

    added authorization

--- lab3/marketplace/urls.py
+++ lab3/marketplace/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from django.contrib import admin
 from .views import *
 
 urlpatterns = [
     path('', HomePage.as_view(), name='home'),
     path('product/<int:pk>/', ViewProduct.as_view(), name='product'),
     path('post_product', PostProduct.as_view(), name='sell'),
-    path('register/', RegisterUser.as_view(), name='register')
-
+    path('register/', RegisterUser.as_view(), name='register'),
+    path('login/', LoginUser.as_view(), name='login'),
+    path('logout/', LogoutUser.as_view(), name='logout'),
 
 ]
--- lab3/marketplace/views.py
+++ lab3/marketplace/views.py
@@ -1,10 +1,8 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView
-
 from marketplace.forms import AddProductForm, RegistrationForm
 from marketplace.models import Product
 
@@ -27,7 +25,7 @@ class ViewProduct(DetailView):
 class PostProduct(LoginRequiredMixin, CreateView):
     form_class = AddProductForm
     template_name = 'marketplace/post_product.html'
-    login_url = reverse_lazy('home')
+    login_url = reverse_lazy('login')
 
 
 class MyProfile(ListView):
@@ -40,4 +38,19 @@ class RegisterUser(CreateView):
     form_class = RegistrationForm
     template_name = 'marketplace/register.html'
     success_url = reverse_lazy('login')
+
+
+class LoginUser(LoginView):
+    form_class = AuthenticationForm
+    template_name = 'marketplace/login.html'
+
+    def get_success_url(self):
+        return reverse_lazy('home')
+
+
+class LogoutUser(LogoutView):
+    template_name = 'registration/logged_out.html'
+    next_page = 'home'
+
+
 

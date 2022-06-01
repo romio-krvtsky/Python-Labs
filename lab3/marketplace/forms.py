667153cebf2723946329fdff879184d9c0efada0
romio-krvtsky <romik.krivetskiy@gmail.com>
2022-06-01 15:01:55 +0300

    updated forms

--- lab3/marketplace/forms.py
+++ lab3/marketplace/forms.py
@@ -7,15 +7,10 @@ from .models import *
 class AddProductForm(forms.ModelForm):
     class Meta:
         model = Product
-        fields = ['name', 'photo', 'state', 'description', 'price', 'owner']
+        fields = ['name', 'photo', 'state', 'description', 'price', ]
 
 
 class RegistrationForm(UserCreationForm):
-    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-input'}))
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-input'}))
-    phone = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-input'}))
-    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-input'}))
-    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-input'}))
 
     class Meta:
         model = User

5a6c19c3b7b912b01e75a4506fa47bdad43e9d50
romio-krvtsky <romik.krivetskiy@gmail.com>
2022-06-01 13:10:52 +0300

    final urls

--- lab3/marketplace/urls.py
+++ lab3/marketplace/urls.py
@@ -3,8 +3,11 @@ from .views import *
 
 urlpatterns = [
     path('', HomePage.as_view(), name='home'),
+    path('myprofile/', MyProfile.as_view(), name='my_profile'),
     path('product/<int:pk>/', ViewProduct.as_view(), name='product'),
-    path('post_product', PostProduct.as_view(), name='sell'),
+    path('product/<int:pk>/edit', EditProduct.as_view(), name='edit_product'),
+    path('product/<int:pk>/delete', DeleteProduct.as_view(), name='delete_product'),
+    path('post_product/', PostProduct.as_view(), name='sell'),
     path('register/', RegisterUser.as_view(), name='register'),
     path('login/', LoginUser.as_view(), name='login'),
     path('logout/', LogoutUser.as_view(), name='logout'),

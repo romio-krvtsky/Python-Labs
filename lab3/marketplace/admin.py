f62310a1e6cef149ad1ad9516a7fb15fb62c3a0b
romio-krvtsky <romik.krivetskiy@gmail.com>
2022-06-01 14:57:22 +0300

    update admin.py

--- lab3/marketplace/admin.py
+++ lab3/marketplace/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
 from .models import *
 
+
 # Register your models here.
-admin.site.register(Product)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'email', 'phone',)
+    list_display_links = ('username',)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'state', 'post_date', 'owner',)
+    list_display_links = ('name',)
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Product, ProductAdmin)
